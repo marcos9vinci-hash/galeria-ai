e4ebfdabe936728712ad1e11414b70c3a06d0742
Marcos Vinicius <marcos9vincipessoal@gmail.com>
2026-07-07 15:04:09 -0300

    Build for deploy

---
+++ assets/CalendarView-bvpJj9k2.js
@@ -0,0 +1,1 @@
+import{j as s}from"./vendor-data-Ta3hzCoa.js";const i=({children:r})=>s.jsx("div",{className:"grid gap-6 md:grid-cols-[1fr,minmax(300px,400px)]",children:r});export{i as CalendarView};
